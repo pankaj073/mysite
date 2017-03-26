ab969a0f10feaaf5b64fcb05d1b0f57394619667
Pankaj Sharma <pankaj073@gmail.com>
2017-03-26 08:50:30 +0000

    modified the functional test, uses unittest module now:

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,18 @@
 from selenium import webdriver
-browser = webdriver.PhantomJS();
-browser.get('http://localhost:8000')
-assert 'Polls' in browser.title, "Browser title was " + browser.title
-browser.quit()
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.PhantomJS()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get("http://localhost:8000");
+
+        self.assertIn("Polls", self.browser.title);
+        self.fail("Finish the test!");
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
